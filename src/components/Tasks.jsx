02c791e13363c781d38ee865a48f46b6395d7cf9
rafael-milani <rafamilanimarangoni@gmail.com>
2025-07-07 20:44:26 -0300

    add-adicionando check ao clicar em tarefas para finaliza-las

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,10 +18,11 @@ function Tasks({ tasks, onTaskClick, onDeleteClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`text-left w-full bg-slate-400 text-white p-2 rounded-md 
+            className={`text-left w-full flex items-center gap-2 bg-slate-400 text-white p-2 rounded-md 
               ${task.isCompleted && "line-through"} 
               `}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onDetailClick(task)}>
